d9760954dcd8b55cb765319319c2f86a0a56d426
MichalMarrow <marrowmichal@gmail.com>
2025-04-09 14:47:56 -0400

    start project

---
+++ crud/server/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./model/Users');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+mongoose.connect(
+  'mongodb+srv://marrowmichal:MpasswordK@crud.oagln7s.mongodb.net/'
+);
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+db.once('open', () => {
+  console.log('Successfully connected to MongoDB!');
+});
+
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected!');
+});
+
+db.on('reconnected', () => {
+  console.log('MongoDB reconnected!');
+});
+
+db.on('connecting', () => {
+  console.log('Connecting to MongoDB...');
+});
+
+app.get('/', (req, res) => {
+  UserModel.find({})
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
+
+app.get('/getUser/:id', (req, res) => {
+  const id = req.params.id;
+  UserModel.findById({ _id: id })
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
+
+app.post('/createUser', (req, res) => {
+  UserModel.create(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.put('/updateUser/:id', (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndUpdate(
+    { _id: id },
+    { name: req.body.name, email: req.body.email, age: req.body.age }
+  )
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
+
+app.delete('/deleteUser/:id', (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndDelete({ _id: id })
+    .then((res) => res.json(res))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log('Server is running.');
+});
